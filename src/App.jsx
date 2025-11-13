792715d606cdba00c7faa96a219def3378a7a84d
Anniballi-Andrea <andreanniballi@gmail.com>
2025-11-13 16:51:23 +0100

    add the complete form

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,20 @@ import { useState } from "react"
 
 
 function App() {
-  const [name, setName] = useState("")
-  const [title, setTitle] = useState("")
-  const [message, seMessage] = useState("")
-  const [checkbox, setCheckbox] = useState(false)
+
+
+  const [formData, setFormData] = useState({
+    author: '',
+    title: '',
+    body: '',
+    public: false
+  })
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    console.log(formData)
+  }
+
 
 
 
@@ -15,32 +25,39 @@ function App() {
         <div className="row">
           <div className="col-sm-12">
             <div className="card">
-              <div className="form pt-3 px-3">
+              <form className=" pt-3 px-3" onSubmit={handleSubmit}>
                 <div>
                   <input
                     type="text " className=" info mt-2" placeholder="Insert your name"
-                    value=""
-                    onChange={setName} />
+                    name="author"
+                    value={formData.author}
+                    onChange={(e) => setFormData({ ...formData, author: e.target.value })} />
                 </div>
                 <div>
                   <input type="text " className="info mt-2" placeholder="Insert tiket title"
-                    value=""
-                    onChange={setTitle} />
+                    name="title"
+                    value={formData.title}
+
+                    onChange={(e) => setFormData({ ...formData, title: e.target.value })} />
                 </div>
                 <div>
                   <input type="text" className="form-control mt-2" placeholder="Insert your tiket message"
-                    value=""
-                    onChange={seMessage} />
+                    name='body'
+                    value={formData.body}
+                    onChange={(e) => setFormData({ ...formData, body: e.target.value })} />
                 </div>
                 <div>
                   <input type="checkbox" className="form-check-input mt-2"
-                    value="false"
+                    name="public"
+                    checked={formData.public}
+                    onChange={(e) => setFormData({ ...formData, public: e.target.checked })}
                   /> public
                 </div>
-                <div>
-                  <button className="btn btn-primary mt-2">Submit</button>
-                </div>
-              </div>
+
+                <button type="submit" className="btn btn-primary mt-2"
+                >Submit</button>
+
+              </form>
             </div>
           </div>
         </div>
